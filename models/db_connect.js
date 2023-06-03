9b48def3c78aa31c4f132d955bbf5d8c179a4ef5
Xcoder-Creator <59529270+Xcoder-Creator@users.noreply.github.com>
2023-06-03 12:07:28 +0100

    Update db_connect.js

--- models/db_connect.js
+++ models/db_connect.js
@@ -3,13 +3,13 @@ const mysql = require('mysql'); // Import mysql module
 // Create mysql connection pool
 const pool  = mysql.createPool({
     connectionLimit : process.env.CONNECTION_LIMIT,
-    host: 'mysql-121756-0.cloudclusters.net',
+    host: 'mysql-129813-0.cloudclusters.net',
     //host            : process.env.HOST,
     user            : process.env.USER,
-    password        : 'fy4uragp',
+    password        : 't6n0Gz8n',
     database        : process.env.DATABASE,
     charset         : process.env.CHARSET,
-    port: 10018,  
+    port: 19840,  
     multipleStatements: true
 });
 //---------------------------------------
